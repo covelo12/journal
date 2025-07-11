08dab52442598242a04d9671a29c76720dace9c7
covelo12 <ricardocovelo11@ua.pt>
2025-07-11 17:03:56 +0100

    try 2

--- fileserver/fileserver.py
+++ fileserver/fileserver.py
@@ -10,7 +10,7 @@ def post_page():
 @app.route('/news')
 def news_page():
     return send_from_directory(app.static_folder, 'news.html')
-
+#test
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
